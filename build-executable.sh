ac5b1a535f03ccde4e19740b8e94f0b186573a21
Eric Würbel <wurbel@univ-tln.fr>
2014-07-03 12:27:11 +0200

    Added a -e option to build-executable in order to embed foreign libraries in the standalone executable.

--- build-executable.sh
+++ build-executable.sh
@@ -1,7 +1,30 @@
 #! /bin/bash
 
-swipl --stand_alone=true --goal="go,halt" --toplevel="halt(1)" -o plrsf -c loader.pl
-swipl --stand_alone=true --goal="true" --toplevel="run" -o plrsf-webdemo -c webstarter.pl
+function usage() {
+	echo "syntaxe : $0 [-e]" >&2
+	echo >&2
+	echo "Builds a standalone executabe for plrsf and plrsf webdemo."  >&2
+	echo >&2
+	echo "options:" >&2
+	echo "-e\tSpecifies that external libs should be embeded in the executable" >&2
+}	
+
+
+ext=false
+while getopts "e" o; do
+    case "$o" in
+	e)   ext=true;;
+	[?]) usage; exit 1;;
+    esac
+done
+
+opts=""
+if [ "$ext" = "true" ] ; then
+    opts="--foreign=save"
+fi
+
+swipl $opts --stand_alone=true --goal="go,halt" --toplevel="halt(1)" -o plrsf -c loader.pl
+swipl $opts --stand_alone=true --goal="true" --toplevel="run" -o plrsf-webdemo -c webstarter.pl
 echo "==========================================================================="
 echo "Compilation terminée,"
 echo "l'exécutable en ligne de commande est disponible dans le fichier plrsf,"
